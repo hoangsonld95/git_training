8029e8f01acc989652ffc270844ccbe25fb7614f
ledinhhoangson <ledinhhoangson316@gmail.com>
2017-05-19 09:47:31 +0700

    Basic task list

---
+++ resources/views/tasks.blade.php
@@ -0,0 +1,75 @@
+<!-- resources/views/tasks.blade.php -->
+
+@extends('layouts.app')
+
+@section('content')
+
+    <!-- Bootstrap Boilerplate... -->
+
+    <div class="panel-body">
+        <!-- Display Validation Errors -->
+        @include('common.errors')
+
+        <!-- New Task Form -->
+        <form action="{{ url('task') }}" method="POST" class="form-horizontal">
+            {{ csrf_field() }}
+
+            <!-- Task Name -->
+            <div class="form-group">
+                <label for="task" class="col-sm-3 control-label">Task</label>
+
+                <div class="col-sm-6">
+                    <input type="text" name="name" id="task-name" class="form-control">
+                </div>
+            </div>
+
+            <!-- Add Task Button -->
+            <div class="form-group">
+                <div class="col-sm-offset-3 col-sm-6">
+                    <button type="submit" class="btn btn-default">
+                        <i class="fa fa-plus"></i> Add Task
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <!-- TODO: Current Tasks -->
+
+    <div class="panel-body">
+
+        <table class="table table-stripped task-table">
+
+            <thead>
+                <th>ID</th>
+                <th>Name</th>
+            </thead>
+            
+            <tbody>
+                @foreach($tasks as $task)
+                    <tr>
+                        <td>{{ $task -> id }}</td>
+                        <td>{{ $task -> name }}</td>
+                        <td>
+                            <form action="{{ url('task/'.$task->id) }}" method="POST">
+                            
+                                {{ csrf_field() }}
+                                {{ method_field('DELETE') }}
+
+                                <button type="submit" class="btn btn-danger">
+                                <i class="fa fa-trash"></i> Delete
+                                </button>
+
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+                
+            </tbody>    
+            
+        </table>
+        
+
+    </div>
+
+@endsection
